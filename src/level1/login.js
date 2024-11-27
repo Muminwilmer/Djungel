71947895ae533ff044b5305c688ace6b63c8ad93
Muminwilmer <wilmer.heltborg2008@gmail.com>
2024-11-27 10:14:47 +0100

    changes

--- src/level1/login.js
+++ src/level1/login.js
@@ -7,7 +7,7 @@ function validateLogin() {
   const encodedPassword = "766af62a6275002e9909af31d1f15e02609d9443de336c0ce13ba52cb3e56042";
   
   if (sha256(password) === encodedPassword){
-    window.location.href = "../level2/level2.html"
+    window.location.href = "../level2/corridor.html"
     return true;
   } else {
     console.log("Wrong password")
